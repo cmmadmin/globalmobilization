68b1d671bb938b60461f58bf9e8efe9a0cfddada
Malachi Wurpts <admin@mobilization.org>
2015-06-25 13:35:46 -0500

    Add filters for force login. This is critical for the www.globalmobilization.org and globalmobilization.org links to work and redirect correctly with the force login plugin.

--- wp-content/themes/thekeynote-v1-03-child/functions.php
+++ wp-content/themes/thekeynote-v1-03-child/functions.php
@@ -1,1 +1,25 @@
-<?php add_filter('show_admin_bar', '__return_false'); ?>
+<?php add_filter('show_admin_bar', '__return_false'); 
+
+/**
+ * Set the URL to redirect to on login.
+ *
+ * @return string URL to redirect to on login. Must be absolute.
+ **/
+function my_forcelogin_redirect() {
+  return site_url();
+}
+add_filter('v_forcelogin_redirect', 'my_forcelogin_redirect', 10, 1);
+
+/**
+ * Filter Force Login to allow exceptions for specific URLs.
+ *
+ * @return array An array of URLs. Must be absolute.
+ **/
+function my_forcelogin_whitelist() {
+   return array(
+     site_url( '/login/' )
+   );
+}
+add_filter('v_forcelogin_whitelist', 'my_forcelogin_whitelist', 10, 1);
+
+?>
