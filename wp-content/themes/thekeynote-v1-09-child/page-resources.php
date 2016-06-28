ea78bea90b218b211a8ed2df384ca007245ff44b
Ian Fraiser <ian.fraiser@mobilization.org>
2016-06-28 11:27:50 -0500

    First site for the GMN as a whole. Also included pages for GMC 2017. Original version had a water droplet dropping into an ocean. A snapshot of its associated database is at \\cmmbackup\Projects\CMM\Web\globalmobilization.org\Development\Backup\2016-06-28-Staging-WaterDropPrototype

---
+++ wp-content/themes/thekeynote-v1-09-child/page-resources.php
@@ -0,0 +1,15 @@
+<?php
+/**
+Template Name: Resources
+ */
+
+get_header(); ?>
+
+	<?php globmob_resource_browse(); ?>
+
+		<div id="resource-content"></div>	
+
+	</section>
+	</div>
+	
+<?php get_footer(); ?>
