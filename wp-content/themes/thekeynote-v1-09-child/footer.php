ac5bfa9a7414df55fde906adecd30dbca35bc35a
Malachi Wurpts <admin@mobilization.org>
2017-02-01 12:50:45 -0600

    Added reftagger for bible verse pop-ups.

--- wp-content/themes/thekeynote-v1-09-child/footer.php
+++ wp-content/themes/thekeynote-v1-09-child/footer.php
@@ -52,5 +52,19 @@ if( empty($gdlr_post_option) || empty($gdlr_post_option['page-style']) ||
 <?php } // page style ?>
 </div> <!-- body-wrapper -->
 <?php wp_footer(); ?>
+
+<script>
+	var refTagger = {
+		settings: {
+			bibleVersion: "ESV"
+		}
+	};
+	(function(d, t) {
+		var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+		g.src = "//api.reftagger.com/v2/RefTagger.js";
+		s.parentNode.insertBefore(g, s);
+	}(document, "script"));
+</script>
+
 </body>
 </html>
